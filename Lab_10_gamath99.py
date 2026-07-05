e751fcbd57efef4462fd1a4b02552b785f916068
gamath99 <gamamathurin1@gmail.com>
2026-07-05 19:27:43 -0400

    Create class wordanalyzer

--- Lab_10_gamath99.py
+++ Lab_10_gamath99.py
@@ -8,6 +8,40 @@ from pathlib import Path
 import string
 
 class Wordanalyzer:
+    def __int__(self, filepath):
+        self.filepath = Path(filepath)
+        self.__frequencies = {}
+
+    def process_file(self):
+        try:
+            if not self.__filepath.exists():
+                raise FileNotFoundError
+            
+            punctuation_table = str.maketrans("", "", string.punctuation)
+
+            with self.__filepath.open("r", encoding ="utf-8") as file:
+                for line in file:
+                    line = line.lower()
+                    line = line.translate(punctuation_table)
+                    words = line.split()
+
+                    for word in words:
+                        if word in self.__frequencies:
+                            self.__frequencies[word] += 1
+                        else:
+                            self.__frequencies[word] = 1
+
+            return True
+        except FileNotFoundError:
+            print(f"Error: The file '{self.__filepath}' was not found.")
+            return False
+        
+    def print_report(self):
+        words = sorted(self.__frequencies.keys())
+
+        for word in words: 
+            print(f"{word:<15}::{self.__frequencies[word]}")
+
 
 
 
