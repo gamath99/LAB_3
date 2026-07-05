fca6b05b92ada0bdc251fce18fa0bbf07b021b88
gamath99 <gamamathurin1@gmail.com>
2026-07-05 19:25:55 -0400

    Add the text files in the repo

---
+++ Lab_10_gamath99.py
@@ -0,0 +1,13 @@
+"""
+Word count project 
+Gama MATHURIN
+The program is meant to analyze word frequency in one of four predefined text files selected by the user. 
+July 5, 2026
+"""
+from pathlib import Path
+import string
+
+class Wordanalyzer:
+
+
+
